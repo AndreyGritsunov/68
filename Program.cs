c824faa285493ccd1e1ba1b294cc13d7fe2d6a0d
unknown <gritsunov85@gmail.com>
2022-09-09 22:38:46 +0600

    end1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число N: ");
+int m = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите число M: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+int sum = RecursionTotal(n, m);
+
+Console.Write(sum);
+
+
+
+static int RecursionTotal(int n, int m)
+{
+  if (n == 0){
+
+    return m + 1;
+
+  }
+  else{
+    if ((n != 0) && (m == 0)){
+
+      return RecursionTotal(n - 1, 1);
+    }
+    else{
+
+      return RecursionTotal(n - 1, RecursionTotal(n, m - 1));
+    }
+  }
+}
+
